2e48f0fbc9086c4696b8faca1b5d835c22bde88c
Rafael Moreira <rmoreira@demandbase.com>
2015-05-02 12:59:15 -0700

    fixing path to healthcheck script

--- check.go
+++ check.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-    "bytes"
-    "io"
-    "log"
-    "os/exec"
-    "net/http"
+	"bytes"
+	"io"
+	"log"
+	"net/http"
+	"os/exec"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    cmd := exec.Command("/usr/local/opt/go/src/github.com/rmoreira/net/healthcheck")
-    var out bytes.Buffer
-    cmd.Stdout = &out
-    err := cmd.Run()
-    if err != nil {
-      log.Fatal(err)
-    }
-    io.WriteString(w, out.String())
+	cmd := exec.Command("/usr/local/opt/go/src/github.com/rmoreira/go-stuff/healthcheck")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	io.WriteString(w, out.String())
 }
 
 func main() {
-    http.HandleFunc("/healthcheck", handler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/healthcheck", handler)
+	http.ListenAndServe(":8080", nil)
 }
